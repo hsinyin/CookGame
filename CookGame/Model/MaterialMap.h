7c5d2c6850e9ba0e35cdae87321bc56f56a198b4
hsinyin <chouhsinyin@gmail.com>
2013-06-23 00:23:26 -0700

    Create MaterialMap

---
+++ CookGame/Model/MaterialMap.h
@@ -0,0 +1,13 @@
+//
+//  MaterialMap.h
+//  CookGame
+//
+//  Created by Hsin-Yin on 6/23/13.
+//  Copyright (c) 2013 Hsin-Yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MaterialMap : NSObject
+
+@end
---
+++ CookGame/Model/MaterialMap.m
@@ -0,0 +1,13 @@
+//
+//  MaterialMap.m
+//  CookGame
+//
+//  Created by Hsin-Yin on 6/23/13.
+//  Copyright (c) 2013 Hsin-Yin. All rights reserved.
+//
+
+#import "MaterialMap.h"
+
+@implementation MaterialMap
+
+@end
